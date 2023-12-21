5058575945723e8d85b14edd37c12697a2836c54
eyad <eyadmohamedelzeky@gmail.com>
2023-12-21 03:11:00 +0200

    add bloc providers

--- lib/app.dart
+++ lib/app.dart
@@ -3,9 +3,12 @@ import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:taskassginment/app/favourite_screen/domain/usecases/favourite_use_case.dart';
+import 'package:taskassginment/app/favourite_screen/presentation/cubit/favourite_screen_cubit.dart';
 import 'package:taskassginment/config/app_routes.dart';
 import 'package:taskassginment/core/commen_cubits/vaild_emil_cubit/vaild_email_cubit.dart';
 import 'package:taskassginment/core/commen_cubits/visiable_password_cubit/cubit/visiable_password_cubit.dart';
+import 'package:taskassginment/core/di/di.dart';
 import 'package:taskassginment/core/localization/generated/l10n.dart';
 import 'package:taskassginment/core/localization/localization_helper.dart';
 
@@ -18,6 +21,10 @@ class MyApp extends StatelessWidget {
       providers: [
         BlocProvider(create: (_) => VisiablePasswordCubit()),
         BlocProvider(create: (_) => VaildEmailCubit()),
+        BlocProvider(
+            create: (_) => FavouriteScreenCubit(
+                  favouriteUseCase: di<FavouriteUseCase>(),
+                )),
       ],
       child: ScreenUtilInit(
         child: MaterialApp.router(
