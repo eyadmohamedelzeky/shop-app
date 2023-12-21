45fabe335fde56ca205604e0822e30d73a70b9fd
eyad <eyadmohamedelzeky@gmail.com>
2023-12-21 03:10:31 +0200

    no

---
+++ lib/app/favourite_screen/data/models/favourite_request_model.g.dart
@@ -0,0 +1,13 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'favourite_request_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Map<String, dynamic> _$$FavouriteRequestModelImplToJson(
+        _$FavouriteRequestModelImpl instance) =>
+    <String, dynamic>{
+      'product_id': instance.productId,
+    };
