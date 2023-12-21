efb366ba1cb8f2dc7b03a5a5999d1ed1119c8843
eyad <eyadmohamedelzeky@gmail.com>
2023-12-21 18:37:16 +0200

    finsihed search product

--- lib/app/home_screen/presentation/screens/home_screen.dart
+++ lib/app/home_screen/presentation/screens/home_screen.dart
@@ -6,30 +6,20 @@ import 'package:taskassginment/app/home_screen/domain/entities/product_response_
 import 'package:taskassginment/app/home_screen/domain/usecases/product_use_case.dart';
 import 'package:taskassginment/app/home_screen/presentation/cubit/home_screen_cubit.dart';
 import 'package:taskassginment/app/product_details_screen/presentation/screen/product_details_screen.dart';
+import 'package:taskassginment/app/search_product/domain/entities/search_product_request_entity.dart';
+import 'package:taskassginment/app/search_product/presentation/cubit/search_product_cubit.dart';
 import 'package:taskassginment/core/commen_widget/custom_text_form_field.dart';
 import 'package:taskassginment/core/di/di.dart';
 import 'package:taskassginment/core/localization/localization.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
-
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List<DataResponeProductEntity> productsToShow = [];
   TextEditingController searchProduct = TextEditingController();
-  List<DataResponeProductEntity> filterProducts(
-      String query, List<DataResponeProductEntity> products) {
-    query = searchController.text;
-    return products.where((product) {
-      return product.name.toLowerCase().contains(query.toLowerCase());
-    }).toList();
-  }
-
-  TextEditingController searchController = TextEditingController();
-
   bool isFavorite = false;
   final products = ProductresponseEntity().data?.data;
   final List<DataResponeProductEntity> favProduct = [];
@@ -40,6 +30,8 @@ class _HomeScreenState extends State<HomeScreen> {
         productUseCase: di<ProductUseCase>(),
       )..fetchProducts(),
       child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+            onPressed: () {}, child: Icon(Icons.logout_rounded)),
         backgroundColor: Colors.orangeAccent,
         appBar: AppBar(
           leading: IconButton(
@@ -53,7 +45,7 @@ class _HomeScreenState extends State<HomeScreen> {
             },
             icon: Icon(Icons.favorite_rounded),
           ),
-          backgroundColor: Colors.blueAccent,
+          backgroundColor: Colors.pinkAccent,
           centerTitle: true,
           title: Text(
             context.localization.products,
@@ -71,134 +63,202 @@ class _HomeScreenState extends State<HomeScreen> {
               );
             } else if (state is ProductsLoaded) {
               final products = state.productresponseEntity.data!.data!;
-              final allProducts = state.productresponseEntity;
-              final filteredProducts =
-                  filterProducts(searchController.text, products);
-              // final productsToShow =
-              //    searchedProduct.isNotEmpty ? searchedProduct : products;
-
               return Column(
                 children: [
                   CustomTextFormField(
-                      textEditingController: searchController,
+                      textEditingController: searchProduct,
                       onChanged: (value) {
-                        productsToShow = filterProducts(searchController.text,
-                            state.productresponseEntity.data!.data!);
-                        setState(() {});
+                        setState(() {
+                          value = searchProduct.text;
+                          BlocProvider.of<SearchProductCubit>(context)
+                              .searchProduct(
+                                  searchProductRequestEntity:
+                                      SearchProductRequestEntity(text: value));
+                        });
                       },
                       labelText: '',
                       hintText: 'search For Products'),
                   SizedBox(
                     height: 20.h,
                   ),
-                  Expanded(
-                    child: ListView.separated(
-                      // itemCount: products.length,
-                      itemCount: filteredProducts.length,
-                      // searchController.text.isEmpty
-                      //     ? products.length
-                      //     : productsToShow.length,
-                      separatorBuilder: (context, index) => SizedBox(
-                        height: 20.h,
-                      ),
-                      itemBuilder: (context, index) {
-                        final productloaded = state.productresponseEntity;
-
-                        return Container(
-                          margin: const EdgeInsets.symmetric(horizontal: 10),
-                          width: double.infinity,
-                          decoration: BoxDecoration(
-                              color: Colors.blueGrey,
-                              borderRadius: BorderRadius.circular(10.r)),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              InkWell(
-                                onTap: () {
-                                  Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                        builder: (context) =>
-                                            ProductDetailsScreen(
-                                          index: index,
-                                          product: ProductresponseEntity(
-                                              data: DataResponseEntity(
-                                            data: productloaded.data!.data!,
-                                          )),
-                                        ),
-                                      ));
-                                },
-                                child: ClipRRect(
-                                  borderRadius: BorderRadius.circular(10.r),
-                                  child: Image.network(
-                                    productloaded.data!.data![index].image,
-                                    // productloaded.image,
-                                    width: double.infinity,
-                                    height: 300.h,
-                                    fit: BoxFit.cover,
-                                  ),
-                                ),
-                              ),
-                              SizedBox(
-                                height: 15.h,
-                              ),
-                              Text(
-                                productloaded.data!.data![index].name ?? '',
-                                //  productloaded.name ?? 'eyad',
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 20.sp),
-                              ),
-                              SizedBox(
-                                height: 15.h,
-                              ),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                children: [
-                                  IconButton(
-                                    onPressed: () {
-                                      setState(() {
-                                        final currentProduct =
-                                            productloaded.data!.data![index];
-                                        if (favProduct
-                                            .contains(currentProduct)) {
-                                          favProduct.remove(currentProduct);
-                                        } else {
-                                          favProduct.add(currentProduct);
-                                        }
-                                      });
-                                    },
-                                    icon: favProduct.contains(
-                                            productloaded.data!.data![index])
-                                        ? Icon(
-                                            Icons.favorite_sharp,
-                                            color: Colors.red,
-                                          )
-                                        : Icon(
-                                            Icons.favorite_border_outlined,
-                                            color: Colors.white,
+                  searchProduct.text.isNotEmpty
+                      ? Expanded(
+                          child: BlocBuilder<SearchProductCubit,
+                              SearchProductState>(
+                            builder: (context, state) {
+                              if (state is SearchProductLoading) {
+                                return Center(
+                                  child: CircularProgressIndicator.adaptive(),
+                                );
+                              } else if (state is SearchProductLoaded) {
+                                final filterproduct =
+                                    state.searchProductEntity.data?.data;
+                                return ListView.separated(
+                                  itemBuilder: (context, index) {
+                                    return Container(
+                                      margin: const EdgeInsets.symmetric(
+                                          horizontal: 10),
+                                      width: double.infinity,
+                                      decoration: BoxDecoration(
+                                          color: Colors.blueGrey,
+                                          borderRadius:
+                                              BorderRadius.circular(10.r)),
+                                      child: Column(
+                                        children: [
+                                          ClipRRect(
+                                            borderRadius:
+                                                BorderRadius.circular(10.r),
+                                            child: Image.network(
+                                              filterproduct?[index].image,
+                                              width: double.infinity,
+                                              height: 300.h,
+                                              fit: BoxFit.cover,
+                                            ),
                                           ),
-                                  )
-                                ],
-                              ),
-                              Text(
-                                'New Price : ${productloaded.data!.data![index].price} EGP',
-                                // 'New Price: ${productloaded.price} EGP',
-                                style: TextStyle(
-                                    color: Colors.green, fontSize: 15.sp),
-                              ),
-                              Text(
-                                'discount: ${productloaded.data!.data![index].discount} %',
-                                // 'discount: ${productloaded.discount} %',
-                                style: TextStyle(
-                                    color: Colors.deepPurple, fontSize: 15.sp),
-                              ),
-                            ],
+                                          SizedBox(
+                                            height: 15.h,
+                                          ),
+                                          Text(
+                                            filterproduct![index].name ?? '',
+                                            textAlign: TextAlign.center,
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 20.sp),
+                                          ),
+                                          SizedBox(
+                                            height: 15.h,
+                                          ),
+                                          Text(
+                                            'New Price : ${filterproduct[index].price} EGP',
+                                            style: TextStyle(
+                                                color: Colors.green,
+                                                fontSize: 15.sp),
+                                          ),
+                                        ],
+                                      ),
+                                    );
+                                  },
+                                  itemCount: filterproduct?.length ?? 0,
+                                  separatorBuilder: (context, index) =>
+                                      SizedBox(
+                                    height: 10.h,
+                                  ),
+                                );
+                              }
+                              return Container();
+                            },
                           ),
-                        );
-                      },
-                    ),
-                  ),
+                        )
+                      : Expanded(
+                          child: ListView.separated(
+                            itemCount: products.length,
+                            separatorBuilder: (context, index) => SizedBox(
+                              height: 10.h,
+                            ),
+                            itemBuilder: (context, index) {
+                              final productloaded = state.productresponseEntity;
+                              return Container(
+                                margin:
+                                    const EdgeInsets.symmetric(horizontal: 10),
+                                width: double.infinity,
+                                decoration: BoxDecoration(
+                                    color: Colors.blueGrey,
+                                    borderRadius: BorderRadius.circular(10.r)),
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    InkWell(
+                                      onTap: () {
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                              builder: (context) =>
+                                                  ProductDetailsScreen(
+                                                index: index,
+                                                product: ProductresponseEntity(
+                                                    data: DataResponseEntity(
+                                                  data:
+                                                      productloaded.data!.data!,
+                                                )),
+                                              ),
+                                            ));
+                                      },
+                                      child: ClipRRect(
+                                        borderRadius:
+                                            BorderRadius.circular(10.r),
+                                        child: Image.network(
+                                          productloaded
+                                              .data!.data![index].image,
+                                          // productloaded.image,
+                                          width: double.infinity,
+                                          height: 300.h,
+                                          fit: BoxFit.cover,
+                                        ),
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 15.h,
+                                    ),
+                                    Text(
+                                      productloaded.data!.data![index].name ??
+                                          '',
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 20.sp),
+                                    ),
+                                    SizedBox(
+                                      height: 15.h,
+                                    ),
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.end,
+                                      children: [
+                                        IconButton(
+                                          onPressed: () {
+                                            setState(() {
+                                              final currentProduct =
+                                                  productloaded
+                                                      .data!.data![index];
+                                              if (favProduct
+                                                  .contains(currentProduct)) {
+                                                favProduct
+                                                    .remove(currentProduct);
+                                              } else {
+                                                favProduct.add(currentProduct);
+                                              }
+                                            });
+                                          },
+                                          icon: favProduct.contains(
+                                                  productloaded
+                                                      .data!.data![index])
+                                              ? Icon(
+                                                  Icons.favorite_sharp,
+                                                  color: Colors.red,
+                                                )
+                                              : Icon(
+                                                  Icons
+                                                      .favorite_border_outlined,
+                                                  color: Colors.white,
+                                                ),
+                                        )
+                                      ],
+                                    ),
+                                    Text(
+                                      'New Price : ${productloaded.data!.data![index].price} EGP',
+                                      style: TextStyle(
+                                          color: Colors.green, fontSize: 15.sp),
+                                    ),
+                                    Text(
+                                      'discount: ${productloaded.data!.data![index].discount} %',
+                                      style: TextStyle(
+                                          color: Colors.deepPurple,
+                                          fontSize: 15.sp),
+                                    ),
+                                  ],
+                                ),
+                              );
+                            },
+                          ),
+                        ),
                 ],
               );
             }
